978c4d0cd9d306d78f3dd528fe63b5822b1d79ee
Hawk <italia195@outlook.it>
2020-05-05 10:33:30 +0200

    Added custom ActionCommand

---
+++ WuxiaReader.Interface/Commands/ActionCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WuxiaReader.Interface.Commands
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action _executeFunction;
+        private readonly Func<bool> _canExecute;
+        
+        public ActionCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _executeFunction = execute;
+            _canExecute = canExecute;
+        }
+        
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeFunction();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+    
+    public class ActionCommand<T> : ICommand
+    {
+        private readonly Action<T> _executeFunction;
+        private readonly Predicate<T> _canExecute;
+        
+        public ActionCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            _executeFunction = execute;
+            _canExecute = canExecute;
+        }
+        
+        public bool CanExecute(object parameter)
+        {
+            if (!(parameter is T param))
+                throw new ArgumentException(nameof(parameter));
+            
+            return _canExecute == null || _canExecute(param);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!(parameter is T param))
+                throw new ArgumentException(nameof(parameter));
+            
+            _executeFunction(param);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
